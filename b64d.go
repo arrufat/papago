97f39385c4f00df7fa5fe57d99f60a22f2e99713
Adrià Arrufat <1671644+arrufat@users.noreply.github.com>
2019-01-15 12:41:44 +0900

    add base64 decoding script

---
+++ b64d.go
@@ -0,0 +1,25 @@
+// build +ignore
+
+// decode papago http payload from base64
+
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a base64 string.")
+		return
+	}
+	str := os.Args[1]
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Printf("%q\n", data)
+}
